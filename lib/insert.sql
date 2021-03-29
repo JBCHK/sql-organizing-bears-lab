3f0d67d3558e30940825f739407f6a2e928232d3
Jonathan Block <blockjonathan29@gmail.com>
2021-03-29 14:38:24 -0400

    commit tree 89e6149d9a3c20bb6c839b5309434957d058c099

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate",21,"M","black","cool",1),
+(2,"Tabitha",10,"F","violet","clairvoyant",1),
+(3,null,20,"M","white","manic",0),
+(4,"Sergeant Brown",13,"M","brown","friendly",1),
+(5,"Melissa",19,"F","golden","pure",0),
+(6,"Rowdy",14,"M","black","loud",1),
+(7,"Grinch",12,"M","green","moody",1),
+(8,"Wendy",9,"F","brown","enthusiastic",1);
